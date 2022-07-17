59c9e341f59bc5e909fdc22968d65d6b1b29f288
Snuffy2 <Snuffy2@users.noreply.github.com>
2022-07-16 22:19:27 -0400

    Cleanup

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""Fixtures for tests"""
-import pytest
-
-pytest_plugins = "pytest_homeassistant_custom_component"
-
-
-@pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations):
-    yield
